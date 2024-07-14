e7c7b80da484944dc2992e6a8769dd5e69b38e7c
C-W-Z <chenweizhang3021@gmail.com>
2024-07-14 21:36:11 +0800

    update battle page

--- src/pages/Battle.tsx
+++ src/pages/Battle.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import './Battle.css'
-import { OKSquare, Star } from '../components/SVGIcons';
+import { CheckMark, OKSquare, Star } from '../components/SVGIcons';
 import { invoke } from '@tauri-apps/api/tauri';
 import useGlobalContext from '../components/GlobalContext';
 import AutoSizeText from '../components/AutoSizeText';
+import TopButtons from '../components/TopButtons';
 
 interface CandidateProps {
     className?: string;
@@ -49,27 +50,55 @@ function Battle() {
     const globalContext = useGlobalContext();
 
     const [className, setClassName] = useState<string[]>(['', '', '', '', '']);
+    const [confirmShow, setConfirmShow] = useState(false);
+    const [choosedDraw, setChoosedDraw] = useState(false);
+    const [unchoosedDraw, setUnchoosedDraw] = useState(false);
+    const [allDraw, setAllDraw] = useState(false);
+
+    function clickChoosedDraw() {
+        if (allDraw)
+            return;
+        setChoosedDraw(!choosedDraw);
+    }
+
+    function clickUnchoosedDraw() {
+        setUnchoosedDraw(!unchoosedDraw);
+    }
+
     function OnChoose(i: number) {
         return () => {
             let copy = Array.from(className);
             if (copy[i] == '') {
                 copy[i] = 'choose';
-                for (let j = 0; j < copy.length; j++)
+                for (let j = 0; j < playerCount; j++)
                     if (i != j)
                         copy[j] = 'unchoose';
+                setConfirmShow(true);
             }
             else if (copy[i] == 'unchoose') {
                 copy[i] = 'choose';
+                let chooseCount = 0;
+                for (let j = 0; j < playerCount; j++)
+                    if (copy[j] == 'choose')
+                        chooseCount++;
+                if (chooseCount == playerCount) {
+                    setChoosedDraw(true);
+                    setAllDraw(true);
+                }
             }
             else if (copy[i] == 'choose') {
                 copy[i] = 'unchoose';
                 let unchooseCount = 0;
-                for (let j = 0; j < copy.length; j++)
+                for (let j = 0; j < playerCount; j++)
                     if (copy[j] == 'unchoose')
                         unchooseCount++;
-                if (unchooseCount == copy.length)
-                    for (let j = 0; j < copy.length; j++)
+                if (unchooseCount == playerCount) {
+                    for (let j = 0; j < playerCount; j++)
                         copy[j] = '';
+                    setConfirmShow(false);
+                }
+                if (allDraw)
+                    setAllDraw(false);
             }
             setClassName(copy);
         }
@@ -84,11 +113,15 @@ function Battle() {
                 // console.log(chars);
                 battleNum += 1;
                 setCharIds(chars);
+                setClassName(['', '', '', '', '']);
                 // restart animation & set font size
                 setShow(false);
                 setTimeout(() => {
                     setShow(true);
                 }, 0);
+                // reset
+                setConfirmShow(false);
+                setAllDraw(false);
             })
             .catch((e) => { console.error(e) });
     }
@@ -123,14 +156,39 @@ function Battle() {
         setCandidates(list);
     }, [globalContext?.loading, char_ids, className]);
 
+    const confirmClass = confirmShow ? 'confirm' : 'confirm close';
+    const chooseDrawClass = allDraw ? 'checkbox alldraw' : 'checkbox';
+
     return (
         <div className='battle'>
+            <TopButtons></TopButtons>
+            <div className="title">请选择最喜欢的干员 (可复选)</div>
             <div className="candidate-area">
                 {show && candidates}
             </div>
-            <button className='confirm' onClick={pickChars}>
+            <div className="checkboxes">
+                <label className={chooseDrawClass}>
+                    <input type="checkbox"
+                        checked={choosedDraw}
+                        onChange={clickChoosedDraw}
+                    />
+                    <span><CheckMark></CheckMark></span>
+                    选中者之间视为平手
+                </label>
+                {playerCount > 2 &&
+                    <label className="checkbox">
+                        <input type="checkbox"
+                            checked={unchoosedDraw}
+                            onChange={clickUnchoosedDraw}
+                        />
+                        <span><CheckMark></CheckMark></span>
+                        落选者之间视为平手
+                    </label>
+                }
+            </div>
+            <button className={confirmClass} onClick={pickChars}>
                 <OKSquare></OKSquare>
-                确认选择
+                {allDraw ? '确认平手' : '确认选择'}
             </button>
         </div>
     )
